97f1dc7ffc9b48c25681a05b1c3012958d1531d5
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-13 22:21:24 +0530

    day 2 python list practice

--- practice.py
+++ practice.py
@@ -1,5 +1,35 @@
-numbers = [1, 2, 3, 4, 5]
-print("Numbers are:")
-for number in numbers:
-    print(number)
-print("Sum =", sum(numbers))
+fruits = ["Apple", "Banana", "Mango"]
+print("Before:", fruits)
+fruits.append("Orange")
+print("After:", fruits)
+colors = ["Red", "Green", "Blue"]
+print("Before:", colors)
+colors.extend(["Yellow", "Purple"])
+print("After:", colors)
+fruits = ["Apple", "Banana", "Mango"]
+fruits.append("orange")
+fruits.append("grapes")
+print(fruits)
+shopping = ["milk", "bread"]
+shopping.append("eggs")
+print(shopping)
+fruits = ["Apple", "Banana", "Mango"]
+print("Before:", fruits)
+fruits.insert(2, "Orange")
+print("After:", fruits)
+colours = ["Red", "Blue"]
+print("Before:", colours)
+colours.insert(2, "Green")
+print("After:", colours)
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+fruits.remove("Banana")
+print(fruits)
+fruits.pop(1)
+print(fruits)
+animals = ["Dog", "Cat", "Elephant", "Lion"]
+animals.remove("Cat")
+print(animals)
+animals.pop(2)
+print(animals)
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print(len(fruits))
