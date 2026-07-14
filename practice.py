e0e999a48201ffbc129d11e161eb444b0b8d2a3a
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-14 23:39:34 +0530

    completed shopping list manager project

--- practice.py
+++ practice.py
@@ -32,4 +32,35 @@ print(animals)
 animals.pop(2)
 print(animals)
 fruits = ["Apple", "Banana", "Mango", "Orange"]
-print(len(fruits))
+print(len(fruits)) 
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print("Before:", fruits)
+fruits.clear()
+print("After:", fruits)
+colours = ["Red", "Green", "Blue" , "Yellow", "Purple"]
+print(colours.count("Green"))
+animals = ["Dog", "Cat", "Elephant", "Lion"]
+print(animals.index("Elephant"))
+numbers = [10, 20, 30, 20, 40, 50]
+print(len(numbers))
+print(numbers.count(20))
+numbers.remove(20)
+print(numbers)
+print(len(numbers))
+print(numbers.index(40))
+numbers = [10, 20, 30, 40, 50]
+print("Before:", numbers)
+numbers.sort()
+print("After:", numbers)
+fruits = ["Banana", "Apple", "Mango", "Orange"]
+fruits.sort()
+print(fruits)
+names = ["John", "Alice", "Bob", "David"]
+names.sort()
+print(names)
+marks = [85, 92, 78, 90, 88]
+marks.sort()
+print(marks)
+print(len(marks))
+print(marks.count(90))
+print(marks.index(78))
