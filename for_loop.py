dbb449ee9f05cc1e769dd7d1b0ae1da85b800751
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-18 21:04:54 +0530

    Completed Python for loop practice

---
+++ for_loop.py
@@ -0,0 +1,57 @@
+for i in range(2, 8):
+    print(i)
+for i in range(1,10,3):
+    print(i)
+for i in range(10, 0, -2):
+    print(i)
+for i in range(5, 16, 5):
+    print(i)
+for i in range(5):
+    print("Hello Vanshika")
+for i in range(3):
+    print("I Love Python")
+for i in range(5):
+    print("Hello", i)
+for i in range(1, 6):
+    print("Chocolate", i)
+for i in range(3, 9):
+    print(i)
+for i in range(2, 11, 2):
+    print(i)
+for i in range(15, 4, -3):
+    print(i)
+print("Example 1")
+for i in range(1, 11):
+    print(i)
+print("Example 2")
+for i in range(2, 21, 2):
+    print(i)
+print("Example 3")
+for i in range(1, 20, 2):
+    print(i)
+print("Example 4")
+for i in range(10, 0, -1):
+    print(i)
+print("Example 5")
+for i in range(5, 51, 5):
+    print(i)
+print("Example 6")
+for i in range(1, 6):
+    print("Square of", i, "=", i * i)
+print("Example 7")
+for i in range(1, 6):
+    print("Welcome Vanshika", i)
+print("Example 7")
+for i in range(1, 6):
+    print("Welcome Vanshika", i)
+print("Example 8")
+for i in range(5, 0, -1):
+    print(i)
+print("🚀 Blast Off!")
+print("Example 9")
+for i in range(5):
+    print("*")
+print("Example 10")
+for i in range(1, 11):
+    print("2 x", i, "=", 2 * i)
+
