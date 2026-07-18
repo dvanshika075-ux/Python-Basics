bbb0b6bef22336d6cdb57af6fe561ed2e3d654eb
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-18 21:29:13 +0530

    Added while loop examples and practice programs

---
+++ while_loop.py
@@ -0,0 +1,73 @@
+i = 1
+while i <= 5:
+    print(i)
+    i = i + 1
+i = 1
+while i <= 5:
+    print("Hello Vanshika")
+    i = i + 1
+i = 2
+while i <= 10:
+    print(i)
+    i = i + 2
+i = 10
+while i >= 1:
+    print(i)
+    i = i - 1
+i = 1
+while i <= 10:
+    print("7 x", i, "=", 7 * i)
+    i = i + 1
+i = 1
+while i <= 5:
+    print(i * i)
+    i = i + 1
+i = 5
+while i <= 15:
+    print(i)
+    i = i + 1
+i = 50
+while i <= 60:
+    print(i)
+    i = i + 1
+i = 0
+while i <= 30:
+    print(i)
+    i = i + 5
+i = 50
+while i >= 5:
+    print(i)
+    i = i - 5
+i = 1
+while i <= 10:
+    print(i ** 3)
+    i = i + 1
+i = 1
+while i <= 5:
+    print(i, i * i)
+    i = i + 1
+i = 1
+while i <= 5:
+    print("Student", i)
+    i = i + 1
+i = 15
+while i >= 5:
+    print(i)
+    i = i - 1
+i = 1
+while i <= 5:
+    print(i, "*")
+    i = i + 1
+i = 2
+total = 0
+while i <= 10:
+    print(i)
+    total = total + i
+    i = i + 2
+print("Total =", total)
+i = 1
+while i <= 5:
+    print(i)
+    if i == 3:
+        break
+    i = i + 1
