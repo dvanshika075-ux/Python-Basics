90db662478f9180345e54befeec45ecee9fb2a98
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-21 23:06:07 +0530

    Completed Python functions chapter

---
+++ Functions (def).py
@@ -0,0 +1,116 @@
+def show_name():
+    print("Vanshika")
+show_name()
+show_name()
+show_name()
+def hello():
+    print("Hello World")
+hello()
+def welcome():
+    print("Welcome to Python Programming")
+welcome()
+def add():
+    print(10 + 20)
+add()
+def square():
+    print(5 * 5)
+square()
+def message():
+    print("have a good day")
+message()
+message()
+def morning():
+    print("Good Morning")
+
+morning()
+def subject():
+    print("Python Programming")
+
+subject()
+def college():
+    print("SGGSW University")
+
+college()
+def goal():
+    print("To become a good programmer")
+
+goal()
+def smile():
+    print("Keep smiling")
+
+smile()
+smile()
+smile()
+def haircare():
+    print("Haircare is important")
+    print("Use good shampoo and conditioner")
+
+haircare()
+def college_name():
+    print("SGGSW University")
+    print("Department of Computer Science")
+
+college_name()
+def stars():
+    print("*****")
+    print("*****")
+    print("*****")
+
+stars()
+def calculation():
+    print("Addition:", 10 + 5)
+    print("Subtraction:", 10 - 5)
+    print("Multiplication:", 10 * 5)
+    print("Division:", 10 / 5)
+
+calculation()
+def bill():
+    print("Total Bill: $50")
+    print("Thank you for your purchase!")
+
+bill()
+def attendence():
+    print("Attendance is mandatory")
+    print("Please mark your attendance on time")
+
+attendence()
+def motivation():
+    print("Believe in yourself")
+    print("You can achieve anything you set your mind to")
+
+motivation()
+def breakfast():
+    print("Breakfast is the most important meal of the day")
+    print("Make sure to have a healthy breakfast")
+
+breakfast()
+def evening():
+    print("Good Evening")
+    print("Hope you had a productive day")
+
+evening()
+def mobile():
+    print("Mobile phones are useful for communication")
+    print("But excessive use can be harmful")
+
+mobile()
+def exercise():
+    print("Regular exercise is important for good health")
+    print("Try to exercise at least 30 minutes a day")
+
+exercise()
+def youtube():
+    print("YouTube is a popular video-sharing platform")
+    print("It offers a wide range of content for entertainment and education")
+
+youtube()
+def travel():
+    print("Traveling can broaden your horizons")
+    print("It allows you to experience new cultures and places")
+    
+travel()
+def dream():
+    print("Dream big and work hard to achieve your goals")
+    print("Never give up on your dreams")
+
+dream()
---
+++ functions_parameters.py
@@ -0,0 +1,108 @@
+def hello(name):
+    print("Hello", name)
+
+hello("Vanshika")
+def student(name):
+    print(name)
+
+student("Rahul")
+def city(city):
+    print("City:", city)
+
+city("Chandigarh")
+def hello(name):
+    print("Hello", name)
+
+hello("Vanshika")
+hello("Rahul")
+hello("Priya")
+def hello(name):
+    print("Hello", name)
+
+hello("Vanshika")
+hello("Rahul")
+def college(name):
+    print("College:", name)
+
+college("SGGSW University")
+def marks(number):
+    print("Marks:", number)
+
+marks(85)
+def food(item):
+    print("Food Item:", item)
+
+food("Pizza")
+food("Burger")
+food("Pasta")
+def dream(goal):
+    print("My dream is to become a", goal)
+
+dream("Software Engineer")
+dream("Data Scientist")
+def mobile(brand):
+    print("Mobile Brand:", brand)
+
+mobile("Apple")
+mobile("Samsung")
+mobile("OnePlus")
+def student(name, age):
+    print("Name:", name)
+    print("Age:", age)
+
+student("Vanshika", 17)
+def add(a, b):
+    print(a + b)
+
+add(10, 20)
+def add():
+    return 50
+
+x = add()
+
+print(x)
+def marks():
+    return 95
+
+score = marks()
+
+print(score)
+print(score + 5)
+def add(a, b):
+    return a + b
+
+answer = add(20, 30)
+
+print(answer)
+def square(number):
+    return number * number
+
+result = square(6)
+
+print(result)
+def cube(number):
+    return number * number * number
+
+print(cube(3))
+def greeting(name):
+    return "Hello " + name
+
+print(greeting("Vanshika"))
+def marks():
+    return 95
+
+score = marks()
+
+print(score + 5)
+def age():
+    return 17
+
+print(age())
+def college():
+    return "SGGSWU"
+
+print(college())
+def city():
+    return "Punjab"
+
+print(city())
