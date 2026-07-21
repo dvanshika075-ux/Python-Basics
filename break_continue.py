4c4c663c13f6711d2e6753843bad3415df3145ae
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-21 19:01:07 +0530

    Completed break and continue practice

---
+++ break_continue.py
@@ -0,0 +1,88 @@
+for i in range(1, 11):
+    if i == 6:
+        break
+    print(i)
+for i in range(10, 0, -1):
+    if i == 7:
+        break
+    print(i)
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i)
+for i in range(1, 11):
+    if i % 2 != 0:
+        continue
+    print(i)
+for i in range(1, 11):
+    if i == 3:
+        continue
+    if i == 8:
+        break
+    print(i)
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    if i == 9:
+        break
+print(i)
+for i in range(1, 8):
+    if i == 2:
+        continue
+
+    print(i)
+
+    if i == 5:
+        break
+for i in range(1, 11):
+
+    if i % 3 == 0:
+        continue
+
+    if i == 8:
+        break
+
+    print(i)
+for i in range(1, 16):
+    if i % 3 == 0:
+        continue
+
+    if i > 10:
+        break
+
+    print(i)
+for i in range(2, 21, 2):
+    if i == 6:
+        continue
+
+    if i == 14:
+        break
+
+    print(i)
+for i in range(1, 11):
+
+    if i % 2 == 0:
+        continue
+
+    print(i)
+
+    if i == 7:
+        break
+for i in range(15):
+
+    if i < 5:
+        continue
+
+    if i == 11:
+        break
+
+    print(i)
+for i in range(1, 21):
+
+    if i % 2 == 0:
+        continue
+
+    if i % 5 == 0:
+        break
+
+    print(i)
