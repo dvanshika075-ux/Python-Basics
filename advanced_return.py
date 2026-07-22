099eb7c2b3e8bedca093931b77396c641916ff66
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-22 17:15:28 +0530

    Completed advanced return functions practice

---
+++ advanced_return.py
@@ -0,0 +1,82 @@
+def add(a, b):
+    return a + b
+
+result = add(15, 25)
+
+print(result)
+def square(num):
+    return num * num
+
+answer = square(8)
+
+print(answer)
+def fullname(first, last):
+    return first + " " + last
+
+name = fullname("Vanshika", "Dhiman")
+
+print(name)
+def age():
+    return 17
+
+my_age = age()
+
+print(my_age)
+def college():
+    return "SGGSWU"
+
+clg = college()
+
+print(clg)
+def marks():
+    return 95
+
+result = marks()
+
+print(result + 5)
+def subtract(a, b):
+    return a - b
+
+answer = subtract(100, 45)
+
+print(answer * 2)
+def multiply(a, b):
+    return a * b
+
+answer = multiply(12, 5)
+
+print(answer)
+def country():
+    return "India"
+
+place = country()
+
+print(place)
+def cube(num):
+    return num * num * num
+
+result = cube(4)
+
+print(result)
+def movie():
+    return "3 Idiots"
+
+name = movie()
+
+print(name)
+def add(a, b):
+    return a + b
+
+x = add(30, 20)
+
+print(x + 50)
+def student(name, age, city):
+    return name
+
+student("Vanshika", 17, "Jalandhar")
+def add(a, b):
+    return a + b
+
+x = add(30, 20)
+
+print(x + 50)
