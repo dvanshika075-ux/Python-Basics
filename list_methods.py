6e491244d2c13d7acc79b9e90f0333a4b412c2d9
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-26 18:40:43 +0530

    Completed Python list methods practice

---
+++ list_methods.py
@@ -0,0 +1,145 @@
+fruits = ["Apple", "Banana"]
+
+fruits.append("Mango")
+
+print(fruits)
+fruits = ["Apple", "Banana"]
+
+fruits.insert(1, "Orange")
+
+print(fruits)
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.remove("Banana")
+
+print(fruits)
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.pop()
+
+print(fruits)
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.clear()
+
+print(fruits)
+students = ["Rahul", "Vanshika"]
+
+students.append("Priya")
+
+print(students)
+marks = [85, 90, 78]
+
+marks.append(95)
+
+print(marks)
+cities = ["Delhi", "Mumbai"]
+
+cities.append("Jalandhar")
+
+print(cities)
+languages = ["Python", "Java"]
+
+languages.append("C")
+
+print(languages)
+phones = ["Samsung", "Apple"]
+
+phones.append("OnePlus")
+
+print(phones)
+colors = ["Red", "Blue"]
+
+colors.insert(1, "Green")
+
+print(colors)
+colors = ["Red", "Blue"]
+
+colors.insert(1, "Green")
+
+print(colors)
+numbers = [10, 30]
+
+numbers.insert(1, 20)
+
+print(numbers)
+movies = ["KGF", "Animal"]
+
+movies.insert(0, "Pushpa")
+
+print(movies)
+subjects = ["Math", "English"]
+
+subjects.insert(2, "Computer")
+
+print(subjects)
+subjects = ["Math", "English"]
+
+subjects.insert(2, "Computer")
+
+print(subjects)
+days = ["Monday", "Wednesday"]
+
+days.insert(1, "Tuesday")
+
+print(days)
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.remove("Banana")
+
+print(fruits)
+fruits = ["Apple", "Banana", "Mango"]
+
+fruits.remove("Banana")
+
+print(fruits)
+cars = ["BMW", "Audi", "Mercedes"]
+
+cars.remove("Audi")
+
+print(cars)
+animals = ["Dog", "Cat", "Tiger"]
+
+animals.remove("Dog")
+
+print(animals)
+games = ["BGMI", "Minecraft", "Valorant"]
+
+games.remove("Minecraft")
+
+print(games)
+numbers = [1, 2, 3, 4]
+
+numbers.pop()
+
+print(numbers)
+foods = ["Pizza", "Burger", "Pasta"]
+
+foods.remove("Burger")
+
+print(foods)
+cities = ["Delhi", "Mumbai", "Jalandhar"]
+
+cities.pop()
+
+print(cities)
+colors = ["Red", "Blue", "Green"]
+
+colors.pop(1)
+
+print(colors)
+students = ["Rahul", "Priya", "Vanshika"]
+
+students.pop(2)
+
+print(students)
+movies = ["KGF", "Pushpa", "Animal"]
+
+movies.pop(0)
+
+print(movies)
+movies = ["KGF", "Pushpa", "Animal"]
+
+movies.pop(0)
+
+print(movies)
