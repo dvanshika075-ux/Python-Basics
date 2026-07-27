36884ca19bf124abf5637e55ae6922c4f6f218df
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-28 00:07:43 +0530

    Added dictionaries examples

---
+++ Dictionaries.py
@@ -0,0 +1,139 @@
+student = {
+    "name": "Vanshika",
+    "age": 17,
+    "college": "SGGSWU"
+}
+
+print(student)
+mobile = {
+    "brand": "Samsung",
+    "price": 25000,
+    "color": "Black"
+}
+
+print(mobile["brand"])
+print(mobile["price"])
+print(mobile["color"])
+car = {
+    "name": "Mercedes",
+    "model": "GLA",
+    "year": 2025
+}
+
+print(car["name"])
+print(car["year"])
+game = {
+    "name": "GTA 6",
+    "company": "Rockstar",
+    "year": 2026
+}
+
+print(game["company"])
+book = {
+    "title": "Python",
+    "pages": 500,
+    "author": "ABC"
+}
+
+print(book["title"])
+movie = {
+    "name": "Animal",
+    "hero": "Ranbir",
+    "year": 2023
+}
+
+print(movie["hero"])
+college = {
+    "name": "SGGSWU",
+    "city": "Jalandhar",
+    "students": 12000
+}
+
+print(college["city"])
+laptop = {
+    "brand": "HP",
+    "ram": "16GB",
+    "price": 65000
+}
+
+print(laptop["price"])
+student = {
+    "name": "Vanshika",
+    "age": 17
+}
+
+student["college"] = "SGGSWU"
+
+print(student)
+mobile = {
+    "brand": "Samsung"
+}
+
+mobile["price"] = 25000
+
+print(mobile)
+car = {
+    "name": "Mercedes"
+}
+
+car["year"] = 2025
+car["color"] = "Black"
+
+print(car)
+student = {
+    "name": "Vanshika",
+    "age": 17
+}
+
+student["age"] = 18
+
+print(student)
+game = {
+    "name": "BGMI"
+}
+
+game["name"] = "GTA 6"
+
+print(game)
+laptop = {
+    "brand": "HP",
+    "price": 65000
+}
+
+laptop["price"] = 70000
+
+print(laptop)
+book = {
+    "title": "Python"
+}
+
+book["pages"] = 500
+
+print(book)
+book = {
+    "title": "Python"
+}
+
+book["pages"] = 500
+
+print(book)
+college = {
+    "name": "SGGSWU"
+}
+
+college["city"] = "Jalandhar"
+college["students"] = 12000
+
+print(college)
+student = {
+    "name": "Rahul",
+    "age": 18
+}
+
+student["college"] = "SGGSWU"
+
+student["age"] = 19
+
+student["city"] = "Jalandhar"
+
+print(student)
