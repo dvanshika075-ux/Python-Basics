453feb277e1966b21f44be9726f43592e92248ad
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-28 00:25:40 +0530

    Completed Python dictionary methods

---
+++ dictionary_methods.py
@@ -0,0 +1,233 @@
+student = {
+    "name": "Vanshika",
+    "age": 17,
+    "city": "Jalandhar"
+}
+
+print(student.get("name"))
+car = {
+    "brand": "BMW",
+    "year": 2025
+}
+
+print(car.get("brand"))
+phone = {
+    "brand": "Samsung",
+    "price": 25000
+}
+
+print(phone.get("price"))
+movie = {
+    "name": "Animal",
+    "hero": "Ranbir"
+}
+
+print(movie.get("hero"))
+book = {
+    "title": "Python",
+    "pages": 500
+}
+
+print(book.get("pages"))
+student = {
+    "name": "Rahul",
+    "age": 18
+}
+
+print(student.keys())
+college = {
+    "name": "SGGSWU",
+    "city": "Jalandhar"
+}
+
+print(college.keys())
+game = {
+    "name": "BGMI",
+    "type": "Battle Royale"
+}
+
+print(game.keys())
+student = {
+    "name": "Priya",
+    "age": 19
+}
+
+print(student.values())
+laptop = {
+    "brand": "HP",
+    "price": 70000
+}
+
+print(laptop.values())
+fruit = {
+    "name": "Apple",
+    "color": "Red"
+}
+
+print(fruit.values())
+student = {
+    "name": "Rahul",
+    "age": 18
+}
+
+print(student.items())
+car = {
+    "brand": "Audi",
+    "color": "White"
+}
+
+print(car.items())
+book = {
+    "title": "Python",
+    "pages": 500
+}
+
+print(book.items())
+student = {
+    "name": "Vanshika",
+    "age": 17,
+    "course": "AI & ML"
+}
+
+print(student.get("course"))
+print(student.get("age"))
+car = {
+    "brand": "BMW",
+    "year": 2025
+}
+
+print(car.get("color"))
+phone = {
+    "brand": "Samsung",
+    "price": 25000
+}
+
+print(phone.keys())
+print(phone.values())
+movie = {
+    "name": "Animal",
+    "hero": "Ranbir",
+    "year": 2023
+}
+
+print(movie.items())
+college = {
+    "name": "SGGSWU",
+    "city": "Jalandhar",
+    "students": 12000
+}
+
+print(college.get("city"))
+print(college.keys())
+print(college.values())
+book = {
+    "title": "Python",
+    "pages": 500,
+    "author": "ABC"
+}
+
+print(book.get("author"))
+print(book.items())
+game = {
+    "name": "BGMI",
+    "type": "Battle Royale",
+    "players": 100
+}
+
+print(game.get("players"))
+print(game.keys())
+game = {
+    "name": "BGMI",
+    "type": "Battle Royale",
+    "players": 100
+}
+
+print(game.get("players"))
+print(game.keys())
+laptop = {
+    "brand": "HP",
+    "ram": "16GB",
+    "price": 65000
+}
+
+print(laptop.values())
+print(laptop.items())
+student = {
+    "name": "Vanshika",
+    "age": 17
+}
+
+print(student.get("name"))
+print(student.get("city"))
+book = {
+    "title": "Python",
+    "pages": 500,
+    "author": "ABC"
+}
+
+print(book.keys())
+phone = {
+    "brand": "Samsung",
+    "price": 25000
+}
+
+print(phone.values())
+car = {
+    "brand": "BMW",
+    "year": 2025
+}
+
+print(car.items())
+movie = {
+    "name": "Animal",
+    "hero": "Ranbir"
+}
+
+movie["year"] = 2023
+
+print(movie)
+college = {
+    "name": "SGGSWU"
+}
+
+college["city"] = "Jalandhar"
+
+college["students"] = 12000
+
+print(college)
+game = {
+    "name": "BGMI",
+    "players": 100
+}
+
+game["players"] = 120
+
+print(game)
+student = {
+    "name": "Rahul",
+    "age": 18
+}
+
+student["college"] = "SGGSWU"
+
+student["age"] = 19
+
+print(student.get("age"))
+
+print(student.keys())
+
+print(student.values())
+data = {
+    "A": 10,
+    "B": 20
+}
+
+print(data.get("A"))
+
+data["C"] = 30
+
+data["B"] = 40
+
+print(data)
+
+print(data.items())
