b18ad893ccb1ad7103ed8f4d47cb58c09daeb951
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-30 22:04:53 +0530

    Learned Python Set Methods

---
+++ set_methods.py
@@ -0,0 +1,117 @@
+fruits = {"Apple", "Banana"}
+
+fruits.add("Mango")
+
+print(fruits)
+fruits = {"Apple", "Banana"}
+
+fruits.add("Apple")
+
+print(fruits)
+languages = {"Python", "Java"}
+
+languages.add("C++")
+
+print(languages)
+cars = {"BMW", "Audi"}
+
+cars.add("Mercedes")
+
+print(cars)
+cars = {"BMW", "Audi"}
+
+cars.add("Mercedes")
+
+print(cars)
+games = {"BGMI", "Valorant", "Free Fire"}
+
+games.remove("BGMI")
+
+print(games)
+games = {"BGMI", "Valorant", "Free Fire"}
+
+games.remove("BGMI")
+
+print(games)
+fruits = {"Apple", "Banana"}
+
+fruits.discard("Apple")
+
+print(fruits)
+subjects = {"Math", "Science"}
+
+subjects.add("English")
+
+print(subjects)
+fruits = {"Apple", "Banana", "Mango"}
+
+removed = fruits.pop()
+
+print(removed)
+print(fruits)
+games = {"BGMI", "Valorant", "Minecraft"}
+
+item = games.pop()
+
+print(item)
+print(games)
+numbers = {10, 20, 30, 40}
+
+print(numbers.pop())
+print(numbers)
+fruits = {"Apple", "Banana", "Mango"}
+
+fruits.clear()
+
+print(fruits)
+languages = {"Python", "Java", "C++"}
+
+languages.clear()
+
+print(languages)
+set1 = {"Python", "Java"}
+
+set2 = set1.copy()
+
+print(set1)
+print(set2)
+numbers = {10, 20, 30}
+
+copy_numbers = numbers.copy()
+
+print(copy_numbers)
+fruits = {"Apple", "Banana"}
+
+fruits.update({"Mango", "Orange"})
+
+print(fruits)
+a = {1, 2}
+
+b = {3, 4}
+
+a.update(b)
+
+print(a)
+subjects = {"Math"}
+
+subjects.update({"Science", "English", "Python"})
+
+print(subjects)
+a = {1, 2}
+
+b = a.copy()
+
+b.add(3)
+
+print(a)
+print(b)
+colors = {"Red", "Blue"}
+
+colors.clear()
+
+print(colors)
+x = {10, 20}
+
+x.update({30, 40, 50})
+
+print(x)
