d4556f94f6df7d9f0b71747f6e7f58c936af7fe0
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-30 21:14:31 +0530

    Added tuple immutability example

---
+++ tuples.py
@@ -0,0 +1,36 @@
+fruits = ("Apple", "Banana", "Mango")
+
+print(fruits)
+data = ("Vanshika", 17, 92.5, True)
+
+print(data)
+colors = ("Red", "Green", "Blue", "Black")
+
+print(colors[0])
+print(colors[2])
+games = ("BGMI", "Valorant", "Minecraft", "Free Fire")
+
+print(games[-1])
+print(games[-2])
+numbers = (10, 20, 30, 40, 50)
+
+print(numbers[1:4])
+cities = ("Delhi", "Mumbai", "Jalandhar", "Sirhind")
+
+print(len(cities))
+subjects = ("Python", "Math", "English")
+
+for subject in subjects:
+    print(subject)
+movies = ("Animal", "KGF", "Pushpa")
+
+print("KGF" in movies)
+print("RRR" in movies)
+names = ["Rahul", "Priya"]
+
+names[0] = "Vanshika"
+
+print(names)
+names = ("Rahul", "Priya")
+
+names[0] = "Vanshika"
