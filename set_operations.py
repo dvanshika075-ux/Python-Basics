7edf07500e1512c8a906b341bd8009fab80d6148
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-30 22:31:54 +0530

    Learned Python Set Operations

---
+++ set_operations.py
@@ -0,0 +1,50 @@
+a = {1, 2, 3}
+
+b = {3, 4, 5}
+
+print(a.union(b))
+fruits1 = {"Apple", "Banana"}
+
+fruits2 = {"Banana", "Mango"}
+
+print(fruits1.union(fruits2))
+languages1 = {"Python", "Java"}
+
+languages2 = {"C", "Java", "C++"}
+
+print(languages1.union(languages2))
+a = {1, 2, 3}
+
+b = {2, 3, 4}
+
+print(a.intersection(b))
+students1 = {"Rahul", "Vanshika", "Priya"}
+
+students2 = {"Vanshika", "Ankit", "Priya"}
+
+print(students1.intersection(students2))
+a = {1, 2, 3}
+
+b = {2, 3, 4}
+
+print(a.difference(b))
+movies1 = {"Animal", "KGF", "Pushpa"}
+
+movies2 = {"KGF", "Leo"}
+
+print(movies1.difference(movies2))
+a = {1, 2, 3}
+
+b = {2, 3, 4}
+
+print(a.symmetric_difference(b))
+games1 = {"BGMI", "Valorant"}
+
+games2 = {"Valorant", "Minecraft"}
+
+print(games1.symmetric_difference(games2))
+games1 = {"BGMI", "Valorant"}
+
+games2 = {"Valorant", "Minecraft"}
+
+print(games1.symmetric_difference(games2))
