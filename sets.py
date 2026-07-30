13d5bc7d7e93019040239226b06268bf60495323
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-30 21:36:27 +0530

    Completed

---
+++ sets.py
@@ -0,0 +1,75 @@
+fruits = {"Apple", "Banana", "Mango"}
+
+print(fruits)
+numbers = {1, 2, 2, 3, 4, 4, 5}
+
+print(numbers)
+colors = {"Red", "Blue", "Red", "Green", "Blue"}
+
+print(colors)
+games = {"BGMI", "Valorant", "BGMI", "Free Fire"}
+
+print(games)
+cities = {"Delhi", "Mumbai", "Delhi", "Jalandhar"}
+
+print(cities)
+movies = {"Animal", "KGF", "Animal", "Pushpa"}
+
+print(movies)
+subjects = {"Python", "Math", "Python", "English"}
+
+print(subjects)
+phones = {"Samsung", "iPhone", "Samsung", "OnePlus"}
+
+print(phones)
+cars = {"BMW", "Mercedes", "BMW", "Audi"}
+
+print(cars)
+animals = {"Lion", "Tiger", "Lion", "Elephant"}
+
+print(animals)
+marks = {95, 80, 95, 75, 80}
+
+print(marks)
+years = {2025, 2026, 2025, 2027}
+
+print(years)
+languages = {"Python", "Java", "C", "Python", "C++"}
+
+print(languages)
+numbers = {10, 20, 30, 40, 10, 20}
+
+print(numbers)
+flowers = {"Rose", "Lily", "Rose", "Lotus"}
+
+print(flowers)
+foods = {"Pizza", "Burger", "Pizza", "Pasta"}
+
+print(foods)
+college = {"CSE", "AI", "ML", "CSE", "DS"}
+
+print(college)
+a = {1, 1, 2, 3, 4, 4, 5}
+
+print(a)
+b = {"Python", "Java", "Python", "Java", "C"}
+
+print(b)
+c = {100, 200, 300, 100, 400, 200}
+
+print(c)
+States = {"California", "Texas", "California", "Florida"}
+
+print(States)
+singers = {"Adele", "Beyonce", "Adele", "Rihanna"}
+
+print(singers)
+movies = {"Inception", "Interstellar", "Inception", "Tenet"}
+
+print(movies)
+cricketers = {"Virat Kohli", "Rohit Sharma", "Virat Kohli", "MS Dhoni"}
+
+print(cricketers)
+Subjects = {"Math", "Science", "Math", "English"}
+
+print(Subjects)
