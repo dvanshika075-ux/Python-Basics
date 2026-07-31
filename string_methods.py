dc4a726b6835659b2dd465b7cf4b81c72b1abe6d
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-31 23:13:59 +0530

    Completed this

---
+++ string_methods.py
@@ -0,0 +1,119 @@
+name = "vanshika"
+
+print(name.upper())
+city = "JALANDHAR"
+
+print(city.lower())
+movie = "animal park"
+
+print(movie.title())
+word = "python"
+
+print(word.capitalize())
+name = "    Vanshika    "
+
+print(name.strip())
+sentence = "I like Java"
+
+print(sentence.replace("Java", "Python"))
+text = "banana"
+
+print(text.count("a"))
+name = "Vanshika"
+
+print(name.startswith("Van"))
+file = "notes.pdf"
+
+print(file.endswith(".pdf"))
+word = "Programming"
+print(word.find("g"))
+college = "SGGSWU"
+
+print(college.startswith("SGG"))
+print(college.startswith("ABC"))
+file = "notes.pdf"
+
+print(file.endswith(".pdf"))
+print(file.endswith(".txt"))
+text = "Python Programming"
+
+print(text.find("Programming"))
+print(text.find("Java"))
+sentence = "AI AI ML AI"
+
+print(sentence.count("AI"))
+sentence = "I love Java"
+
+new_sentence = sentence.replace("Java", "Python")
+
+print(sentence)
+print(new_sentence)
+name = "vanshika"
+
+print(name.upper())
+name = "VANSHIKA"
+
+print(name.lower())
+sentence = "python programming language"
+
+print(sentence.title())
+name = "   Vanshika   "
+
+print(name)
+print(name.strip())
+subjects = "Python,Java,C++"
+
+print(subjects.split(","))
+subjects = ["Python", "Java", "C++"]
+
+result = " - ".join(subjects)
+
+print(result)
+letters = ["A", "B", "C", "D"]
+
+print("".join(letters))
+name = "Vanshika"
+
+print(name.isalpha())
+age = "18"
+
+print(age.isdigit())
+age = "18Years"
+
+print(age.isdigit())
+username = "Vanshika123"
+
+print(username.isalnum())
+username = "Vanshika 123"
+
+print(username.isalnum())
+text = "     "
+
+print(text.isspace())
+text = " Hello "
+
+print(text.isspace())
+text = "Python Programming"
+
+print(text.swapcase())
+name = "Python"
+
+print(name.center(20))
+print("AI".center(15, "*"))
+name = "Python"
+
+print(name.ljust(15, "-"))
+name = "Python"
+
+print(name.rjust(15, "-"))
+fullname = "   vanshika dhiman   "
+
+print(fullname)
+
+print(fullname.strip())
+
+print(fullname.strip().title())
+
+print(fullname.strip().upper())
+
+print(fullname.strip().lower())
