d8cf9aa5a7c9ace907a2ceddb992f77c5296c4a8
Vanshika Dhiman <dvanshika075@gmail.com>
2026-07-31 22:51:54 +0530

    completed string basic

---
+++ strings.py
@@ -0,0 +1,79 @@
+name = "Vanshika"
+
+print(name)
+city = 'Jalandhar'
+
+print(city)
+college = "SGGSWU"
+
+print(college)
+paragraph = """Hello
+My name is Vanshika.
+I am learning Python."""
+
+print(paragraph)
+language = "Python"
+
+print(language[0])
+print(language[1])
+print(language[2])
+language = "Python"
+
+print(language[-1])
+print(language[-2])
+print(language[-3])
+name = "Vanshika"
+
+print(len(name))
+movie = "Animal"
+
+print(len(movie))
+car = "Mercedes"
+
+print(car[0])
+print(car[-1])
+print(len(car))
+car = "Mercedes"
+
+print(car[0])
+print(car[-1])
+print(len(car))
+word = "Python"
+
+for letter in word:
+    print(letter)
+name = "Vanshika"
+
+for letter in name:
+    print(letter)
+text = "Python"
+
+print("P" in text)
+print("Z" in text)
+city = "Jalandhar"
+
+print("J" in city)
+print("x" in city)
+first = "Vanshika"
+
+last = "Dhiman"
+
+print(first + " " + last)
+print("Python " * 3)
+print("AI " * 5)
+print("My name is \"Vanshika\"")
+print('It\'s raining today.')
+name = "Vanshika"
+
+print(name)
+
+print(name[0])
+
+print(name[-1])
+
+print(len(name))
+
+for letter in name:
+    print(letter)
+
+print("V" in name)
